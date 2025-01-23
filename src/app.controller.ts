d68fb9227a4065373de8cfba6bd3851a11a3a45b
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-01-22 23:46:43 -0300

    feat: update AppController and AppService for status endpoint and add Prisma module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('/app')
+@ApiTags('status')
+@Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getAppStatus();
   }
 }
