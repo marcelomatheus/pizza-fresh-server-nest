b003ae4814ab8f0196e12e731db3d3543445ba9c
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-01-22 23:46:33 -0300

    feat: add global validation and Swagger documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }))
+ 
+  const config = new DocumentBuilder()
+  .setTitle('PizzaFresh')
+  .setDescription('Aplicação para gestão de mesas')
+  .setVersion('1.0.0')
+  .addTag('status')
+  .addTag('table')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
