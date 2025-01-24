dd9ca5798fa9210b0396daf09e4bf7cd17645077
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-01-24 09:55:28 -0300

    feat: validation options CreateProductDto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,19 +1,29 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString, IsUrl } from "class-validator";
 
 export class CreateProductDto {
     @ApiProperty({
         description: 'Dados do produto',
         example: 'À moda'
     })
+    @IsString()
     name: string;
+
     @ApiProperty({
         example: 'Molho, calabresa, peperone e cebola'
     })
+    @IsString()
     description: string;
+
+    @IsNumber({
+        maxDecimalPlaces: 2
+    })
     @ApiProperty({
         example: 48.95
     })
     price: number;
+
+    @IsUrl()
     @ApiProperty({
         example: 'https://image.com/image.png'
     })
