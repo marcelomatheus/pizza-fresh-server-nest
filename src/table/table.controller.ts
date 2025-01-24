1893489e714ab5d73f201944593d7d67a1b5cf2f
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-01-23 23:22:11 -0300

    feat: enhance Table module with CRUD operations and error handling

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,19 +1,51 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Put } from "@nestjs/common";
 import { TableService } from "./table.service";
 import { CreateTableDto } from "./dto/create-table.dto";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Table } from "./entities/table.entity";
 
 @ApiTags('table')
 @Controller('table')
 export class TableController {
-    constructor(private tableService: TableService) {}
+    constructor(private readonly tableService: TableService) {}
     @Get()
-    findAll() {
+    @ApiOperation({
+        summary: 'List all tables'
+    })
+    findAll(): Promise<Table[]> {
         return this.tableService.findAll();
     }
 
+    @Get(':id')
+    @ApiOperation({
+        summary: 'Return a specific table'
+    })
+    findOne(@Param('id') id: string): Promise<Table> {
+        return this.tableService.findOne(id);
+    }
+
     @Post()
-    create(@Body() createTableDto: CreateTableDto) {
+    @ApiOperation({
+        summary: 'Create a data table'
+    })
+    create(@Body() createTableDto: CreateTableDto): Promise<Table> {
         return this.tableService.create(createTableDto);
+    }
+
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    @ApiOperation({
+        summary: 'Remove a table'
+    })
+    async delete(@Param('id') id:string) {
+        await this.tableService.delete(id)
+    }
+
+    @Patch(':id')
+    @ApiOperation({
+        summary: 'Update a data table'
+    })
+    update(@Param('id') id:string, @Body() createTableDto: CreateTableDto): Promise<Table>{
+        return this.tableService.update(id,createTableDto)
     }
 }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,18 +1,61 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
 import { CreateTableDto } from "./dto/create-table.dto";
 import { Table } from "./entities/table.entity";
+import { PrismaService } from "src/prisma/prisma.service";
+import { UpdateTableDto } from "./dto/update-table.dto";
 
 @Injectable()
 export class TableService{
-    tables: Table[] = []
-    create(createTableDto: CreateTableDto) {
-        const table: Table = { id: 'random_id', ...createTableDto}
-
-        this.tables.push(table)
-        return table
+   
+    constructor(private readonly prisma: PrismaService){}
+    async findById(id: string): Promise<Table>{
+        const record =  await this.prisma.table.findUnique({
+            where: {
+                id,
+            }
+        })
+        if(!record) throw new NotFoundException(`O registro com o ID ${id} não foi econtrado`)
+        return record;
     }
-    findAll() {
-        return this.tables
-    }
 
+    create(createTableDto: CreateTableDto): Promise<Table> {
+        const table: Table = {...createTableDto}
+ 
+        return this.prisma.table.create({
+            data: table
+        }).catch(this.handleError)
+    }
+    handleError(error: Error): undefined {
+        const messageLines = error.message.split('\n')
+        const messageError = messageLines[messageLines.length-1].trim()
+        throw new UnprocessableEntityException(messageError || 'Algum erro ocorreu ao executar a operação')
+    }
+    findAll(): Promise<Table[]> {
+        return this.prisma.table.findMany()
+    }
+    async findOne(id: string): Promise<Table> {
+        return await this.findById(id)
+        
+    }
+    async delete(id: string) {
+        await this.findById(id)
+        await this.prisma.table.delete({
+            where: {
+                id
+            }
+        })
+    }
+ 
+    async update(id: string, updateTableDto: UpdateTableDto): Promise<Table> {
+        await this.findById(id)
+
+        const table: Partial<Table> = {...updateTableDto}
+
+        return this.prisma.table.update({
+            data: table,
+            where: {
+                id
+            }
+        }).catch(this.handleError)
+    }
 }
