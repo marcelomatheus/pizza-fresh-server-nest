c083220e7bb95800bf47b5f6355ffc0f6b85bb8b
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-02-05 11:13:19 -0300

    feat: implement JWT in login flow

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prisma: PrismaService, 
+        private readonly jwtService: JwtService
+    ) {}
   async login(loginAuthDto: LoginAuthDto): Promise<LoginResponseDto> {
     const { nickname, password } = loginAuthDto;
     const user = await this.prisma.user.findUnique({
@@ -15,11 +18,18 @@ export class AuthService {
         }
     });
     if (!user) {
-        throw new NotFoundException('User not found');
+        throw new NotFoundException('Usuário ou senha inválidos');
     }
-    const auth = await bcrypt.compare(password, user.password);
+    const isHashValid = await bcrypt.compare(password, user.password);
+
+    if (!isHashValid) {
+        throw new UnauthorizedException('Usuário ou senha inválidos');
+    }
+
+    delete user.password;
+
     return {
-        token: 'token',
+        token: this.jwtService.sign({payload: nickname}),
         user
     }
   }
