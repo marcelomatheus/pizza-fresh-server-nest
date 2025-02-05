d84ef8ec83f66fcac087ba0cef319b76db9644a8
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-02-04 23:33:42 -0300

    feat(user): implmente userSelect without password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,48 +1,71 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from './entities/user.entity';
-import { find } from 'rxjs';
 import { handleError } from 'src/utils/handle-errors.util';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UserService {
+  private userSelect = {
+    id: true,
+    name: true,
+    createdAt: true,
+    updatedAt: true
+  }
   constructor(private readonly prisma: PrismaService){}
-  async findById(id: string): Promise<User>{
+  async findById(id: string): Promise<Partial<User>>{
       const record =  await this.prisma.user.findUnique({
           where: {
-              id,
-          }
+              id
+          },
+          select: this.userSelect
       })
       if(!record) throw new NotFoundException(`O registro com o ID ${id} não foi econtrado`)
       return record;
   }
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto): Promise<Partial<User>> {
+    if(createUserDto.password !== createUserDto.confirmPassword){
+        throw new UnauthorizedException('As senhas são diferentes')
+    }
     delete createUserDto.confirmPassword
-    const user: User = {...createUserDto}
+    const user: User = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10)
+    }
     return this.prisma.user.create({
-        data: user
+        data: user,
+        select: this.userSelect
     }).catch(handleError)
   }
 
-  findAll(): Promise<User[]> {
-    return this.prisma.user.findMany()
+  findAll(): Promise<Partial<User>[]> {
+    return this.prisma.user.findMany({
+      select: this.userSelect
+    })
   }
 
-  findOne(id: string): Promise<User> {
-    return this.findById(id)
+  async findOne(id: string): Promise<Partial<User>> {
+    const data = await this.findById(id)
+    return data
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<Partial<User>> {
     await this.findById(id)
+    if(updateUserDto.password){
+        if(updateUserDto.password !== updateUserDto.confirmPassword){
+            throw new UnauthorizedException('As senhas são diferentes')
+        }
+        updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10)
+    }
     delete updateUserDto.confirmPassword
     const data: Partial<User> = {...updateUserDto}
     return this.prisma.user.update({
         data,
         where: {
             id
-        }
+        }, select: this.userSelect
     }).catch(handleError)
   }
 
