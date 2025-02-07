f07b31d5ca790f9184e067dd0b63866571ab055a
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-02-07 00:01:34 -0300

    feat: enable CORS for NestJS application and set trust proxy

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   app.enableCors({
-    origin: ['http://192.168.0.5'],
+    origin: 'http://192.168.0.5',
     methods: 'PUT'
   })
+  app.set('trust proxy', 1)
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
     whitelist: true,
