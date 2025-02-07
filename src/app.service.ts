ef2948d0d7e8da2833c724772a6f262457d76b53
Marcelo Oliveira <marcelomatheusbr@gmail.com>
2025-02-07 00:00:15 -0300

    feat: improve status app

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getAppStatus(): string {
-    return 'Server is running';
+  getAppStatus(baseUrl: string): string {
+    return 'Server is running, your IP is from ' + baseUrl;
   }
 }
